c3dae75583b9a08cff6de4d8ad7cedd747b71d10
CallHealth <callhealth@callhealths-mac-mini.local>
2015-10-21 10:26:59 +0530

    Initial Commit

---
+++ ClientApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClientApp
+//
+//  Created by CallHealth on 21/10/15.
+//  Copyright © 2015 CallHealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClientApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ClientApp
+//
+//  Created by CallHealth on 21/10/15.
+//  Copyright © 2015 CallHealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
